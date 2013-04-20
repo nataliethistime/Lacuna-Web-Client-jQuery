00a48186b6be688f03903e213a3dbdff071f5e39
The New Vasari Collective <thevasari@gmail.com>
2013-04-20 21:03:21 +1000

    Redoing the buildings interface for future work.

--- game.js
+++ game.js
@@ -15,17 +15,41 @@
 				].join(''))
 				
 				// Now for the fun stuff.
-				this.GetBuildings($.Lacuna.GameData.Status.empire.home_planet_id);
+				this.getBuildings($.Lacuna.GameData.Status.empire.home_planet_id);
 			},
 			
-			GetBuildings: function(planetId) {
+			getBuildings: function(id) {
+				var buildingsTemplate = [];
+				for (var x = -5; x < 6; x++) {
+					for (var y = -5; y < 6; y++) {
+						buildingsTemplate[buildingsTemplate.length] = [
+							'<div id="plot_', x, '_', y, '" title="Ground" style="',
+									'width: 100px;',
+									'height: 100px;',
+									'left: ', (parseInt(x) + 5) * 100, 'px;',
+									'top: ', (parseInt(y) - 5) * 100 * -1, 'px;',
+									'position: absolute;',
+							'">',
+							'</div>'
+						].join('');
+					}
+				}
+				//console.log(buildingsTemplate.join('\n'));//debug
 				
+				// Send it to the DOM.
+				$('#buildingsParent').html([
+					'<div id="buildingsDraggableChild">',
+					buildingsTemplate.join(''),
+					'</div>'
+				].join(''));
+				
+				// Right, now that that's out of the way, onward we must go...
 				$.Lacuna.send({
 					module: '/body',
 					method: 'get_buildings',
 					params: [
 						$.Lacuna.GetSession(),
-						planetId
+						id
 					],
 					
 					success: function(o) {
@@ -33,36 +57,24 @@
 							body      = o.result.body;
 							keys      = Object.keys(buildings),
 							content   = [];
-						
+							
 						for (var i = 0; i < keys.length; i++) {
 							var buildingId = keys[i],
-								building   = buildings[buildingId];
-							
-							content[content.length] = [
-								'<div id="plot_', building.x, '_', building.y, '" style="',
-									// Yes, I have long-line-itis. No, I'm not doing anything about it.
-									'background: url(\'', window.assetsUrl, '/planet_side/100/', building.image, '.png\') no-repeat transparent;',
-									'width: 100px;',
-									'height: 100px;',
-									'left: ', (parseInt(building.x) + 5) * 100, 'px;',
-									'top: ', (parseInt(building.y) - 5) * 100 * -1, 'px;',
-									'position: absolute;',
-								'" title="', building.name, '">',
-								'</div>'
-							].join('');
+								building   = buildings[buildingId]
+								el         = $('#plot_' + parseInt(building.x) + '_' + parseInt(building.y));
+								
+							// Woopsie! Long line alert!!
+							el.css('background', 'url(\'' + window.assetsUrl + '/planet_side/100/' + building.image + '.png\') no-repeat transparent');
+							el.attr('title', building.name);
 						}
 						
 						$('#lacuna').css({
 							'background-image' : 'url(\'' + window.assetsUrl + '/planet_side/' + body.surface_image + '.jpg\')'
 						});
-						$('#buildingsParent').html([
-							'<div id="buildingsDraggableChild">',
-								content.join(''),
-							'</div>'
-						].join('')).ready(function() {
-							$('#buildingsDraggableChild').draggable({
-								scroll: false
-							});
+						
+						// Start the Draggable.
+						$('#buildingsDraggableChild').draggable({
+							scroll: false
 						});
 					}
 				});
